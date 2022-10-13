bfb1896b234f1217b394502a8b4808fdbeec6cb6
Donald W. Boulton <donaldboulton@gmail.com>
2022-10-13 14:42:49 -0500

    useHasMounted on Map

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -4,6 +4,7 @@ import { MapContainer, TileLayer, Circle, LayerGroup, FeatureGroup, Rectangle, P
 import 'leaflet-fullscreen/dist/Leaflet.fullscreen.js'
 import 'leaflet-fullscreen/dist/leaflet.fullscreen.css'
 import useConfigureLeaflet from '@/hooks/useConfigureLeaflet'
+import useHasMounted from '@/hooks/useHasMounted'
 import MarkerHome from './MarkerHome'
 import MarkerWork from './MarkerWork'
 import MarkerHangOut from './MarkerHangOut'
@@ -16,6 +17,7 @@ if (isBrowser) {
 
 function Map() {
   useConfigureLeaflet()
+  const hasMounted = useHasMounted()
   const mapRef = useRef(null)
   const center = [35.590607, -97.43913]
   const rectangle = [
@@ -30,40 +32,43 @@ function Map() {
   if (typeof window !== 'undefined') {
     return (
       <div>
-        <MapContainer
-          className="markercluster-map"
-          center={center}
-          zoom={11}
-          fullscreenControl={true}
-          scrollWheelZoom={false}
-          whenCreated={map => {
-            mapRef.current = map
-          }}
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <LayerGroup>
-            <Circle center={center} pathOptions={fillBlueOptions} radius={300} />
-            <Circle center={center} pathOptions={fillRedOptions} radius={100} stroke={false} />
+        {useHasMounted && (
+          <MapContainer
+            className="markercluster-map"
+            center={center}
+            zoom={11}
+            fullscreenControl={true}
+            scrollWheelZoom={false}
+            whenCreated={map => {
+              mapRef.current = map
+            }}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
             <LayerGroup>
-              <Circle center={[35.609775, -97.549442]} pathOptions={greenOptions} radius={200} />
+              <Circle center={center} pathOptions={fillBlueOptions} radius={300} />
+              <Circle center={center} pathOptions={fillRedOptions} radius={100} stroke={false} />
+              <LayerGroup>
+                <Circle center={[35.609775, -97.549442]} pathOptions={greenOptions} radius={200} />
+              </LayerGroup>
             </LayerGroup>
-          </LayerGroup>
-          <FeatureGroup pathOptions={purpleOptions}>
-            <Popup>My Will Rodgers Park Hangout</Popup>
-            <Circle center={[35.501429, -97.581642]} radius={100} />
-            <Rectangle bounds={rectangle} />
-          </FeatureGroup>
-          <MarkerHome />
-          <MarkerWork />
-          <MarkerHangOut />
-        </MapContainer>
+            <FeatureGroup pathOptions={purpleOptions}>
+              <Popup>My Will Rodgers Park Hangout</Popup>
+              <Circle center={[35.501429, -97.581642]} radius={100} />
+              <Rectangle bounds={rectangle} />
+            </FeatureGroup>
+            <MarkerHome />
+            <MarkerWork />
+            <MarkerHangOut />
+          </MapContainer>
+        )}
       </div>
     )
   } else {
     return <div className="h-96" />
   }
 }
+
 export default Map
