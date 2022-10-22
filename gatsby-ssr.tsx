c77879725512e2d1f3be0f6c43d1651ae6350428
Donald W. Boulton <donaldboulton@gmail.com>
2022-10-22 07:45:56 -0500

    by by flash

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -13,7 +13,7 @@ export const wrapPageElement: GatsbySSR['wrapPageElement'] = ({ element }) => {
 
 export const wrapRootElement = wrap
 
-export function onRenderBody({ setHeadComponents, setHtmlAttributes }) {
+export function onRenderBody({ setHeadComponents, setPreBodyComponents, setHtmlAttributes }) {
   if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') return null
   setHtmlAttributes({ lang: 'en' })
   setHeadComponents([
@@ -29,5 +29,42 @@ export function onRenderBody({ setHeadComponents, setHtmlAttributes }) {
         gtag('config', '${GATSBY_GA_MEASUREMENT_ID}', { send_page_view: false })`,
       }}
     />,
+  ])
+  setPreBodyComponents([
+    React.createElement('script', {
+      key: 'gatsby-dark-mode',
+      dangerouslySetInnerHTML: {
+        __html: `
+void function() {
+  window.__onThemeChange = function() {}
+  var preferredTheme
+  try {
+    preferredTheme = localStorage.getItem('theme')
+  } catch (err) { }
+  function setTheme(newTheme) {
+    if (preferredTheme && document.documentElement.classList.contains(preferredTheme)) {
+      document.documentElement.classList.replace(preferredTheme, newTheme)
+    } else {
+      document.documentElement.classList.add(newTheme)
+    }
+    window.__theme = newTheme
+    preferredTheme = newTheme
+    window.__onThemeChange(newTheme)
+  }
+  window.__setPreferredTheme = function(newTheme) {
+    setTheme(newTheme)
+    try {
+      localStorage.setItem('theme', newTheme)
+    } catch (err) {}
+  }
+  var darkQuery = window.matchMedia('(prefers-color-scheme: dark)')
+  darkQuery.addListener(function(e) {
+    window.__setPreferredTheme(e.matches ? 'dark' : 'light')
+  })
+  setTheme(preferredTheme || (darkQuery.matches ? 'dark' : 'light'))
+}()
+    `,
+      },
+    }),
   ])
 }
