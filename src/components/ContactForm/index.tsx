dd0c139c5ae45045279a1d2525097521feeffdfd
Donald W. Boulton <donaldboulton@gmail.com>
2022-10-26 15:08:33 -0500

    do do

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -65,7 +65,8 @@ const ContactForm: FC<ContactFormProps> = props => {
         <NetlifyFormComponent onSubmit={handleSubmit(onSubmit)} enableRecaptcha>
           <>
             <Honeypot />
-            <Recaptcha siteKey={SITE_RECAPTCHA_KEY} invisible />
+            <Recaptcha siteKey={SITE_RECAPTCHA_KEY} 
+            invisible />
             <p className="hidden">
               <label>
                 Don not fill this out if you are human: <input name="bot-field" />
