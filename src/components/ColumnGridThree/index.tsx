748c392e61521ff5bab931fab835f4b212230cb2
Donald W. Boulton <donaldboulton@gmail.com>
2023-01-12 14:40:48 -0600

    tiny to images

--- src/components/ColumnGridThree/index.tsx
+++ src/components/ColumnGridThree/index.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 const ColumnGridThree = ({ children }) => {
   return (
     <>
-     <div className="grid grid-flow-row grid-flow-col auto-rows-max border border-l-2 border-rose-900 rounded-lg px-2 grid-rows-3 sm:grid-rows-1 gap-y-2 items-center">
+     <div className="grid grid-flow-row grid-flow-col auto-rows-max border border-l-2 border-pink-500 rounded-lg px-2 grid-rows-3 sm:grid-rows-1 gap-y-2 items-center">
         {children}
       </div>
     </>
