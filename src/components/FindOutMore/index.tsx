fb28219133bff00d14bb93887f09af50c0e8c673
Donald W. Boulton <donaldboulton@gmail.com>
2023-05-05 09:04:53 -0500

    model updates

--- src/components/FindOutMore/index.tsx
+++ src/components/FindOutMore/index.tsx
@@ -7,7 +7,7 @@ import WavyHr from '@/components/WavyHr'
 
 const callouts = [
   {
-    name: 'Gatsby v5.8.1',
+    name: 'Gatsby v5.9.0',
     description: 'Gatsby is a React-based open-source framework for creating websites and apps.',
     imageSrc: GatsbyImage,
     imageAlt: 'Gatsby drawing',
